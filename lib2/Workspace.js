e06970fb823d392fa2fbf6c71751a99d4b3f4844
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-07-28 18:50:25 -0300

    feat(Workspace attachServer): add event listeners
    Default event listeners fire when new client connects, when client is ready and when client disconnects.
    List of all clients is also automatically updates.

--- lib2/Workspace.js
+++ lib2/Workspace.js
@@ -1,8 +1,13 @@
 var EventEmitter = require('events').EventEmitter,
 
-   util = require('./util');
+   util = require('./util'),
+   StorageCreator = require('./Store'),
+   ClientCreator = require('./Client');
 
-var WORKSPACE_EVENTS = util.WORKSPACE_EVENTS,
+var SERVER_EVENTS = util.SERVER_EVENTS,
+   WORKSPACE_EVENTS = util.WORKSPACE_EVENTS,
+   WORKSPACE_OBJECT_EVENTS = util.WORKSPACE_OBJECT_EVENTS,
+   CLIENT_EVENTS = util.CLIENT_EVENTS,
    DEFAULT_SHAPE = {
       x: 0,
       y: 0,
@@ -24,6 +29,7 @@ function Workspace(racer) {
    this.id = model.id();
    this.type = TYPE;
    this.shape = util.clone(DEFAULT_SHAPE);
+   this.allConnectedClients = new StorageCreator();
    this.clients = [];
    this.inner = [];
 
@@ -46,7 +52,37 @@ util.merge(Workspace.prototype, EventEmitter.prototype);
 Workspace.prototype.attachServer = function(srv) {
    this.srv = srv;
 
+   var client,
+      _this = this;
    //srv listeners
+   this.srv.on(SERVER_EVENTS.connectClient, function(err, socket) {
+      if (err) {
+         _this.emit(WORKSPACE_EVENTS.clientConnected, err);
+         _this.emit(WORKSPACE_EVENTS.clientReady, err);
+      } else {
+         client = new ClientCreator(_this.racer, socket);
+         _this.allConnectedClients.push(client);
+
+         _this.emit(WORKSPACE_EVENTS.clientConnected, null, client);
+
+         client.on(CLIENT_EVENTS.ready, function(err) {
+            if (err) {
+               _this.emit(WORKSPACE_EVENTS.clientReady, err);
+            } else {
+               _this.emit(WORKSPACE_EVENTS.clientReady, null, client);
+            }
+         });
+      }
+   });
+   this.srv.on(SERVER_EVENTS.disconnectClient, function(err, socket) {
+      if (err) {
+         _this.emit(WORKSPACE_EVENTS.clientDisconnected, err);
+      } else {
+         client = _this.allConnectedClients.remove(socket);
+
+         _this.emit(WORKSPACE_EVENTS.clientDisconnected, null, client);
+      }
+   });
 };
 
 module.exports = Workspace;
