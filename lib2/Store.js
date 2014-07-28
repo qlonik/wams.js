94d6d3ff2d6d0b2727e686840113928c40c8f911
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-07-28 18:10:25 -0300

    feat(Store find): find() returns element from Store by property
    Given property, we return element from Store. Property can be that one
    that can be passed to equal function of Client class.

--- lib2/Store.js
+++ lib2/Store.js
@@ -6,4 +6,10 @@ function Store() {
 Store.prototype = Array.prototype;
 
 
+Store.prototype.find = function(property) {
+   return util.find(this, function(el) {
+      return el.equal(property);
+   });
+};
+
 module.exports = exports = Store;
