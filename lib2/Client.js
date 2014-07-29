26ca035869048d12112e6da1fe1c535788bd9bfc
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-07-29 15:04:26 -0300

    feat(Client): expand Client object with fields

--- lib2/Client.js
+++ lib2/Client.js
@@ -2,6 +2,22 @@ var EventEmitter = require('events').EventEmitter,
 
    util = require('./util');
 
+var SERVER_EVENTS = util.SERVER_EVENTS,
+   WORKSPACE_EVENTS = util.WORKSPACE_EVENTS,
+   WORKSPACE_OBJECT_EVENTS = util.WORKSPACE_OBJECT_EVENTS,
+   CLIENT_EVENTS = util.CLIENT_EVENTS,
+   SOCKET_EVENTS = util.SOCKET_EVENTS,
+   HAMMER_EVENTS = util.HAMMER_EVENTS,
+   DEFAULT_SHAPE = {
+      x: 0,
+      y: 0,
+      w: 0,
+      h: 0,
+      r: 0,       // rotation
+      s: 100      // scale
+   },
+   TYPE = 'client';
+
 function Client(racer, socket) {
    EventEmitter.call(this);
    this.racer = racer;
@@ -12,6 +28,12 @@ function Client(racer, socket) {
       path = racer.path;
 
    this.id = model.id();
+   this.type = TYPE;
+   this.clientModelPath = path + '.clients.' + this.id;
+   this.shape = util.clone(DEFAULT_SHAPE);
+   this.workspaces = [];
+
+   this.clientModel = model.at(this.clientModelPath);
 
 }
 
