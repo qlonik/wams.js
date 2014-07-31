b6fe1a60587505b85b14497f04776c3ea2631354
Nikita Volodin [Никита Володин] <volodin.n@gmail.com>
2014-07-31 13:33:07 -0300

    feat(JSON2HTML): allow variable metadata fields
    Now we can specify in which fields will HTML metadata will be stored.

--- lib2/client/JSON2HTML.js
+++ lib2/client/JSON2HTML.js
@@ -38,13 +38,15 @@ inner is optional
 /**
  * Takes one json object and returns one html element
  * @param metadata
+ * @param opts
  * @returns {HTMLElement}
  */
-function createAndPopulateNode(metadata) {
-   var node = document.createElement(metadata.tag);
+function createAndPopulateNode(metadata, opts) {
+   var tag = opts.tag, attr = opts.attr, style = opts.style, inner = opts.inner,
+      node = document.createElement(metadata[tag]);
 
-   if (util.isPlainObject(metadata.attr)) {
-      util.forOwn(metadata.attr, function (value, key) {
+   if (util.isPlainObject(metadata[attr])) {
+      util.forOwn(metadata[attr], function (value, key) {
          if (util.isArray(value)) {
             value = value.join(' ');
          }
@@ -52,23 +54,23 @@ function createAndPopulateNode(metadata) {
       });
    }
 
-   if (util.isPlainObject(metadata.style)) {
-      util.forOwn(metadata.style, function (value, key) {
+   if (util.isPlainObject(metadata[style])) {
+      util.forOwn(metadata[style], function (value, key) {
          node.style[key] = value;
       });
    }
 
    function createAndAppendChild(child) {
-      var childNode = createAndPopulateNode(child);
+      var childNode = createAndPopulateNode(child, opts);
       node.appendChild(childNode);
    }
 
-   if (util.isString(metadata.inner)) {
-      node.innerHTML = metadata.inner;
-   } else if (util.isPlainObject(metadata.inner)) {
-      createAndAppendChild(metadata.inner);
-   } else if (util.isArray(metadata.inner)) {
-      util.forEach(metadata.inner, createAndAppendChild);
+   if (util.isString(metadata[inner])) {
+      node.innerHTML = metadata[inner];
+   } else if (util.isPlainObject(metadata[inner])) {
+      createAndAppendChild(metadata[inner]);
+   } else if (util.isArray(metadata[inner])) {
+      util.forEach(metadata[inner], createAndAppendChild);
    }
 
    return node;
